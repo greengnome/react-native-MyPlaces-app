b84700307ef1583c0a08bda2c10d0c25487e7852
Kirill Gladkov <crosfire2011@gmail.com>
2018-01-20 12:06:42 +0200

    added responsive design to login page and place detail page

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,11 @@
-import React, {Component} from 'react';
-import {View, Text, Button, TextInput, StyleSheet, ImageBackground} from 'react-native';
+import React, { Component } from 'react';
+import {View, 
+        Text, 
+        Button, 
+        TextInput, 
+        StyleSheet, 
+        ImageBackground, 
+        Dimensions} from 'react-native';
 
 import startMainTabs from './../MainTabs/startMainTabs';
 import DefaultAppInput from '../../components/UI/DefaultAppInput/DefaultAppInput';
@@ -12,23 +18,67 @@ import forestImg from '../../assets/forest.jpg';
 import NonBackgroundButton from '../../components/UI/NonBackgroundButton/NonBackgroundButton';
 
 class AuthScreen extends Component {
+    DIMANTIONS_ENUM = {
+        landscape: 'landscape',
+        portrait: 'portrait'
+    };
+
+    state = {
+        viewMode: Dimensions.get('window').height > 500 ? this.DIMANTIONS_ENUM.portrait : 
+                                                          this.DIMANTIONS_ENUM.landscape  
+    };
+
+    constructor(props) {
+        super(props);
+
+        Dimensions.addEventListener('change', this.updateStyles);
+    };
+
+    componentWillUnmount() {
+        Dimensions.removeEventListener('change', this.updateStyles);
+    };
+
+    updateStyles = (dims) => {
+        this.setState({
+            viewMode: dims.window.height > 500 ? 'portrait' : 'landscape'
+        });
+    };
+
     loginHandler = () => {
         startMainTabs();
-    }
+    };
 
     render() {
+        let headingText = null;
+        if(this.state.viewMode === this.DIMANTIONS_ENUM.portrait) {
+            headingText = (
+                <MainText>
+                    <HeadingText>Please Log In</HeadingText>
+                </MainText>
+            );
+        }
+
         return (
             <ImageBackground source={forestImg} style={styles.bgImage}>
                 <View style={styles.container}>
-                    <MainText>
-                        <HeadingText>Please Log In</HeadingText>
-                    </MainText>
-                    {/* <BackgroundedButton color='#29aaf4'>Switch to login</BackgroundedButton> */}
+                    {headingText}
                     <NonBackgroundButton border='white' onPress={() => alert('Switch')}>Switch to login</NonBackgroundButton>
                     <View style={styles.inputContainer}>
                         <DefaultAppInput placeholderTextColor='white' placeholder='Your e-mail address' style={styles.input} />
-                        <DefaultAppInput placeholderTextColor='white' placeholder='Password' style={styles.input} />
-                        <DefaultAppInput placeholderTextColor='white' placeholder='Confirm password' style={styles.input} />
+                        <View style={this.state.viewMode === this.DIMANTIONS_ENUM.portrait ? 
+                                     styles.portraitPasswordContainer : 
+                                     styles.landscapePasswordContainer}>
+                            <View style={this.state.viewMode === this.DIMANTIONS_ENUM.portrait ?
+                                         styles.portraitPasswordWrapper : 
+                                         styles.landscapePasswordWrapper}>
+                            <DefaultAppInput placeholderTextColor='white' placeholder='Password' style={styles.input} />
+                            </View>
+                            <View style={this.state.viewMode === this.DIMANTIONS_ENUM.portrait ?
+                                         styles.portraitPasswordWrapper : 
+                                         styles.landscapePasswordWrapper}>
+                            <DefaultAppInput placeholderTextColor='white' placeholder='Confirm password' style={styles.input} />
+                            </View>
+                        </View>
                     </View>
                     {/* <BackgroundedButton color='#29aaf4'>Login</BackgroundedButton> */}
                     <NonBackgroundButton border='white' onPress={this.loginHandler}>Login</NonBackgroundButton>
@@ -58,6 +108,20 @@ const styles = StyleSheet.create({
     bgImage: {
         width: '100%',
         flex: 1
+    },
+    landscapePasswordContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    portraitPasswordContainer: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start'
+    },
+    landscapePasswordWrapper: {
+        width: '45%'
+    },
+    portraitPasswordWrapper: {
+        width: '100%'
     }
 })
 
--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
-import { View, Image, Text, Button, TextInput, StyleSheet, TouchableOpacity, Platform} from 'react-native';
+import { View, 
+    Image, 
+    Text, 
+    Button, 
+    TextInput, 
+    StyleSheet, 
+    TouchableOpacity, 
+    Platform, 
+    Dimensions } from 'react-native';
 import { connect } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import { deletePlace } from '../../store/actions/index';
 
 class PlaceDetail extends Component {
+    DIMANTIONS_ENUM = {
+        landscape: 'landscape',
+        portrait: 'portrait'
+    };
+
+    state = {
+        viewMode: Dimensions.get('window').height > 500 ? this.DIMANTIONS_ENUM.portrait : 
+                                                          this.DIMANTIONS_ENUM.landscape
+    }
+
+    constructor(props) {
+        super(props);
+        Dimensions.addEventListener('change', this.updateStyles);
+    }
+
+    updateStyles = dims => {
+        this.setState({
+            viewMode: Dimensions.get('window').height > 500 ? this.DIMANTIONS_ENUM.portrait : 
+                                                              this.DIMANTIONS_ENUM.landscape
+        });
+    }
+
+    componentWillUnmount() {
+        Dimensions.removeEventListener('change', this.updateStyles);
+    }
 
     placeDeletedHandler = () => {
         this.props.onDeletePlace(this.props.selectedPlace.key);
@@ -14,15 +47,21 @@ class PlaceDetail extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <View>
+            <View style={[styles.container, this.state.viewMode === this.DIMANTIONS_ENUM.portrait ? 
+                                            styles.portraitContainer : 
+                                            styles.landscapeContainer]}>
+                <View style={styles.subContainer}>
                     <Image source={this.props.selectedPlace.image} style={styles.placeImage} />
-                    <Text style={styles.placeName} >{this.props.selectedPlace.name}</Text>
                 </View>
-                <View style={styles.detailsButtonContainer}>
-                    <TouchableOpacity onPress={this.placeDeletedHandler}>
-                        <Icon size={30} name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'} color='red' />
-                    </TouchableOpacity>
+                <View style={styles.subContainer}>
+                    <View>
+                        <Text style={styles.placeName} >{this.props.selectedPlace.name}</Text>
+                    </View>
+                    <View style={styles.detailsButtonContainer}>
+                        <TouchableOpacity onPress={this.placeDeletedHandler}>
+                            <Icon size={30} name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'} color='red' />
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
         )
@@ -31,7 +70,14 @@ class PlaceDetail extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        margin: 22
+        margin: 22,
+        flex: 1
+    },
+    portraitContainer: {
+        flexDirection: 'column'
+    },
+    landscapeContainer: {
+        flexDirection: 'row' 
     },
     placeImage: {
         width: "100%",
@@ -43,12 +89,13 @@ const styles = StyleSheet.create({
         fontSize: 28
     },
     detailsButtonContainer: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between"
+        alignSelf: 'center'
     },
     modalButton: {
         width: 150
+    },
+    subContainer: {
+        flex: 1
     }
 });
 
